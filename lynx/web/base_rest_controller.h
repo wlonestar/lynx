26e36e6bf9a2a29e18c6c8cdb4e0078f4024265d
wlonestar <wlonestar@163.com>
2024-07-24 22:26:33 +0800

    simplified controller code

--- lynx/web/base_rest_controller.h
+++ lynx/web/base_rest_controller.h
@@ -7,6 +7,11 @@
 
 #include <map>
 #include <memory>
+#include <nlohmann/json.hpp>
+
+namespace lynx {
+
+using json = nlohmann::json;
 
 void setRespOk(lynx::HttpResponse *resp) {
   resp->setStatusCode(lynx::HttpResponse::Ok200);
@@ -15,9 +20,97 @@ void setRespOk(lynx::HttpResponse *resp) {
   resp->addHeader("Server", "lynx");
 }
 
-namespace lynx {
+template <typename T> struct Result {
+  int status;          // NOLINT
+  std::string message; // NOLINT
+  T data;              // NOLINT
+};
+
+template <typename T>
+Result<T> makeOkResult(const std::string &message, const T &data) {
+  return lynx::Result<T>{200, message, data};
+}
+
+Result<std::string> makeErrorResult(const std::string &message,
+                                    const std::string &hint) {
+  return lynx::Result<std::string>{200, message, hint};
+}
+
+template <typename T> // NOLINTNEXTLINE
+void to_json(json &j, const Result<T> &result) {
+  j["status"] = result.status;
+  j["message"] = result.message;
+  j["data"] = result.data;
+}
+
+template <typename T> // NOLINTNEXTLINE
+void from_json(const json &j, Result<T> &result) {
+  j.at("status").get_to(result.status);
+  j.at("message").get_to(result.message);
+  j.at("data").get_to(result.data);
+}
 
 using HttpHandler = std::function<void(const HttpRequest &, HttpResponse *)>;
+
+#define RequestMapping(Controller, _method_, _path_, func)                     \
+  void func##Handler(const lynx::HttpRequest &req, lynx::HttpResponse *resp) { \
+    setRespOk(resp);                                                           \
+    resp->setBody(func().dump());                                              \
+  }
+
+#define RequestMappingWithPathVariable(Controller, _method_, _path_, func,     \
+                                       path_type)                              \
+  void func##Handler(const lynx::HttpRequest &req, lynx::HttpResponse *resp) { \
+    auto &p = req.path();                                                      \
+    path_type arg;                                                             \
+    if constexpr (std::is_same_v<path_type, int64_t> ||                        \
+                  std::is_same_v<path_type, uint64_t>) {                       \
+      arg = atoll(p.substr(p.find_last_of('/') + 1).c_str());                  \
+    }                                                                          \
+    setRespOk(resp);                                                           \
+    resp->setBody(func(arg).dump());                                           \
+  }
+
+#define RequestMappingWithBody(Controller, _method_, _path_, func, body_type)  \
+  void func##Handler(const lynx::HttpRequest &req, lynx::HttpResponse *resp) { \
+    auto &body = req.body();                                                   \
+    lynx::json j = lynx::json::parse(body);                                    \
+    body_type arg = j;                                                         \
+    setRespOk(resp);                                                           \
+    resp->setBody(func(arg).dump());                                           \
+  }
+
+#define RequestMappingWithPathVariableAndBody(Controller, _method_, _path_,    \
+                                              func, path_type, body_type)      \
+  void func##Handler(const lynx::HttpRequest &req, lynx::HttpResponse *resp) { \
+    auto &p = req.path();                                                      \
+    path_type arg1;                                                            \
+    if constexpr (std::is_same_v<path_type, int64_t> ||                        \
+                  std::is_same_v<path_type, uint64_t>) {                       \
+      arg1 = atoll(p.substr(p.find_last_of('/') + 1).c_str());                 \
+    }                                                                          \
+    auto &body = req.body();                                                   \
+    lynx::json j = lynx::json::parse(body);                                    \
+    body_type arg2 = j;                                                        \
+    setRespOk(resp);                                                           \
+    resp->setBody(func(arg1, arg2).dump());                                    \
+  }
+
+#define RestController(Controller)                                             \
+  inline std::map<std::pair<std::string, std::string>, lynx::HttpHandler>      \
+      Controller##RouteTable;                                                  \
+  void Controller##Register(lynx::WebServer &server) {                         \
+    for (auto &[pair, handler] : Controller##RouteTable) {                     \
+      server.addRoute(pair.first, pair.second, handler);                       \
+    }                                                                          \
+  }
+
+#define RegisterHandler(Controller, _method_, _path_, func)                    \
+  Controller##RouteTable[std::make_pair(_method_, _path_)] =                   \
+      [this](auto &&PH1, auto &&PH2) {                                         \
+        func##Handler(std::forward<decltype(PH1)>(PH1),                        \
+                      std::forward<decltype(PH2)>(PH2));                       \
+      };
 
 } // namespace lynx
 
--- tests/web/student.h
+++ tests/web/student.h
@@ -6,12 +6,8 @@
 #include "lynx/web/base_repository.h"
 #include "lynx/web/base_rest_controller.h"
 
-#include <nlohmann/json.hpp>
-
 #include <optional>
 #include <vector>
-
-using json = nlohmann::json;
 
 enum Gender : int {
   Male,
@@ -28,7 +24,7 @@ struct Student {
 } __attribute__((packed));
 
 // NOLINTNEXTLINE
-void to_json(json &j, const Student &s) {
+void to_json(lynx::json &j, const Student &s) {
   j["id"] = s.id;
   j["name"] = s.name;
   j["gender"] = s.gender;
@@ -38,7 +34,7 @@ void to_json(json &j, const Student &s) {
 }
 
 // NOLINTNEXTLINE
-void from_json(const json &j, Student &s) {
+void from_json(const lynx::json &j, Student &s) {
   j.at("id").get_to(s.id);
   j.at("name").get_to(s.name);
   j.at("gender").get_to(s.gender);
@@ -92,88 +88,6 @@ private:
   StudentRepository repository_;
 };
 
-template <typename T> struct Result {
-  int status;          // NOLINT
-  std::string message; // NOLINT
-  T data;              // NOLINT
-};
-
-template <typename T> // NOLINTNEXTLINE
-void to_json(json &j, const Result<T> &result) {
-  j["status"] = result.status;
-  j["message"] = result.message;
-  j["data"] = result.data;
-}
-
-template <typename T> // NOLINTNEXTLINE
-void from_json(const json &j, Result<T> &result) {
-  j.at("status").get_to(result.status);
-  j.at("message").get_to(result.message);
-  j.at("data").get_to(result.data);
-}
-
-/// For none parameters
-#define RequestMapping(Controller, _method_, _path_, func)                     \
-  void handle##func(const lynx::HttpRequest &req, lynx::HttpResponse *resp) {  \
-    setRespOk(resp);                                                           \
-    resp->setBody(func().dump());                                              \
-  }
-
-/// For path variable
-#define RequestMappingWithPathVariable(Controller, _method_, _path_, func,     \
-                                       path_type)                              \
-  void handle##func(const lynx::HttpRequest &req, lynx::HttpResponse *resp) {  \
-    auto &p = req.path();                                                      \
-    path_type arg;                                                             \
-    if constexpr (std::is_same_v<path_type, int64_t> ||                        \
-                  std::is_same_v<path_type, uint64_t>) {                       \
-      arg = atoll(p.substr(p.find_last_of('/') + 1).c_str());                  \
-    }                                                                          \
-    setRespOk(resp);                                                           \
-    resp->setBody(func(arg).dump());                                           \
-  }
-
-#define RequestMappingWithBody(Controller, _method_, _path_, func, body_type)  \
-  void handle##func(const lynx::HttpRequest &req, lynx::HttpResponse *resp) {  \
-    auto &body = req.body();                                                   \
-    json j = json::parse(body);                                                \
-    body_type arg = j;                                                         \
-    setRespOk(resp);                                                           \
-    resp->setBody(func(arg).dump());                                           \
-  }
-
-#define RequestMappingWithPathVariableAndBody(Controller, _method_, _path_,    \
-                                              func, path_type, body_type)      \
-  void handle##func(const lynx::HttpRequest &req, lynx::HttpResponse *resp) {  \
-    auto &p = req.path();                                                      \
-    path_type arg1;                                                            \
-    if constexpr (std::is_same_v<path_type, int64_t> ||                        \
-                  std::is_same_v<path_type, uint64_t>) {                       \
-      arg1 = atoll(p.substr(p.find_last_of('/') + 1).c_str());                 \
-    }                                                                          \
-    auto &body = req.body();                                                   \
-    json j = json::parse(body);                                                \
-    body_type arg2 = j;                                                        \
-    setRespOk(resp);                                                           \
-    resp->setBody(func(arg1, arg2).dump());                                    \
-  }
-
-#define RestController(Controller)                                             \
-  inline std::map<std::pair<std::string, std::string>, lynx::HttpHandler>      \
-      Controller##route_table;                                                 \
-  void Controller##Register(lynx::WebServer &server) {                         \
-    for (auto &[pair, handler] : Controller##route_table) {                    \
-      server.addRoute(pair.first, pair.second, handler);                       \
-    }                                                                          \
-  }
-
-#define RegisterHandler(Controller, _method_, _path_, func)                    \
-  Controller##route_table[std::make_pair(_method_, _path_)] =                  \
-      [this](auto &&PH1, auto &&PH2) {                                         \
-        handle##func(std::forward<decltype(PH1)>(PH1),                         \
-                     std::forward<decltype(PH2)>(PH2));                        \
-      };
-
 RestController(StudentController);
 class StudentController {
 public:
@@ -189,72 +103,50 @@ public:
   }
 
   RequestMapping(StudentController, "GET", "/student100", selectTop100);
-  json selectTop100() {
-    auto data = service_.selectTop100();
-    Result<std::vector<Student>> result{200, "query success", data};
-    json j = result;
-    return j;
+  lynx::json selectTop100() {
+    return lynx::makeOkResult("query success", service_.selectTop100());
   }
 
   RequestMapping(StudentController, "GET", "/student", selectAll);
-  json selectAll() {
-    auto data = service_.selectAll();
-    Result<std::vector<Student>> result{200, "query success", data};
-    json j = result;
-    return j;
+  lynx::json selectAll() {
+    return lynx::makeOkResult("query success", service_.selectAll());
   }
 
   RequestMappingWithPathVariable(StudentController, "GET", "/student/([0-9]+)",
                                  selectById, uint64_t);
-  json selectById(uint64_t id) {
+  lynx::json selectById(uint64_t id) {
     if (auto data = service_.selectById(id)) {
-      Result<Student> result{200, "query success", *data};
-      json j = result;
-      return j;
+      return lynx::makeOkResult("query success", *data);
     }
-    Result<std::string> result{400, "query fail", "id not exists"};
-    json j = result;
-    return j;
+    return lynx::makeErrorResult("query fail", "id not exists");
   }
 
   RequestMappingWithBody(StudentController, "POST", "/student", insert,
                          Student);
-  json insert(Student student) {
+  lynx::json insert(Student student) {
     if (service_.insert(student)) {
-      Result<Student> result{200, "insert success", student};
-      json j = result;
-      return j;
+      return lynx::makeOkResult("insert success", student);
     }
-    Result<std::string> result{400, "insert fail", "id not exists"};
-    json j = result;
-    return j;
+    return lynx::makeErrorResult("insert fail", "id not exists");
   }
 
   RequestMappingWithPathVariableAndBody(StudentController, "PUT",
                                         "/student/([0-9]+)", updateById,
                                         uint64_t, Student);
-  json updateById(uint64_t id, Student &student) {
+  lynx::json updateById(uint64_t id, Student &student) {
     if (service_.updateById(id, std::move(student))) {
-      Result<std::string> result{200, "update success", "success"};
-      json j = result;
-      return j;
+      return lynx::makeOkResult<std::string>("update success", "success");
     }
-    Result<std::string> result{400, "update fail", "id not exists"};
-    json j = result;
-    return j;
+    return lynx::makeErrorResult("update fail", "id not exists");
   }
 
   RequestMappingWithPathVariable(StudentController, "DELETE",
                                  "/student/([0-9]+)", deleteById, uint64_t);
-  json deleteById(uint64_t id) {
+  lynx::json deleteById(uint64_t id) {
     if (service_.deleteById(id)) {
-      Result<std::string> result{200, "delete success", "success"};
-      json j = result;
-      return j;
+      return lynx::makeOkResult<std::string>("delete success", "success");
     }
-    Result<std::string> result{400, "delete fail", "id not exists"};
-    json j = result;
-    return j;
+    return lynx::makeErrorResult("delete fail", "id not exists");
   }
 
 private:
